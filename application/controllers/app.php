032964ded902de0666c2a7ba414daeeff5963e90
ritotombe <ritotombe@gmail.com>
2020-08-15 21:38:27 +1000

    revamp

--- application/controllers/app.php
+++ application/controllers/app.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class app extends CI_Controller {
+class App extends CI_Controller {
 
 	public function __construct()
     {
