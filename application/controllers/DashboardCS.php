0402d4f26c66a55c73b99ed611e7b002ba4dc510
ritotombe <ritotombe@gmail.com>
2020-08-17 11:50:21 +1000

    manage access

--- application/controllers/DashboardCS.php
+++ application/controllers/DashboardCS.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class DashboardCS extends CI_Controller {
+class Dashboardcs extends CI_Controller {
 
 	public function __construct()
     {
