95e2c0273773e71774e3c22c103687d9a3457c29
ritotombe <ritotombe@gmail.com>
2020-08-26 20:19:39 +1000

    bugfix

--- application/views/kelola_antrian.php
+++ application/views/kelola_antrian.php
@@ -315,7 +315,6 @@ defined('BASEPATH') or exit('No direct script access allowed');
             <script src="<?php echo base_url(); ?>assets/global/scripts/app.min.js" type="text/javascript"></script>
             <!-- END THEME GLOBAL SCRIPTS -->
             <!-- BEGIN PAGE LEVEL SCRIPTS -->
-            <script src="<?php echo base_url(); ?>assets/pages/scripts/dashboard.min.js" type="text/javascript"></script>
             <!-- END PAGE LEVEL SCRIPTS -->
             <!-- BEGIN THEME LAYOUT SCRIPTS -->
             <script src="<?php echo base_url(); ?>assets/layouts/layout2/scripts/layout.min.js" type="text/javascript"></script>
