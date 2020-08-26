a729d35be668f695982a06b074ca0e41d7e80986
ritotombe <ritotombe@gmail.com>
2020-08-26 21:05:19 +1000

    stop video

--- application/views/tampil_antrian_pilih_poli.php
+++ application/views/tampil_antrian_pilih_poli.php
@@ -211,7 +211,6 @@ defined('BASEPATH') or exit('No direct script access allowed');
                 });
             </script>
             <script>
-
                 var tag = document.createElement('script');
 
                 tag.src = "https://www.youtube.com/iframe_api";
@@ -221,35 +220,33 @@ defined('BASEPATH') or exit('No direct script access allowed');
                 var player;
 
                 function onYouTubeIframeAPIReady() {
-                    player = new YT.Player('player');
+                    player = new YT.Player('player', {
+                        events: {
+                            // call this function when player is ready to use
+                            'onReady': onPlayerReady
+                        }
+                    });
                 }
 
                 // 4. The API will call this function when the video player is ready.
                 function onPlayerReady(event) {
                     event.target.playVideo();
+                    window.addEventListener("storage", function() {
+                        if (localStorage.getItem('panggil') == 1) {
+                            pauseVideo();
+                        } else {
+                            playVideo()
+                        }
+                    }, false);
                 }
 
-                // 5. The API calls this function when the player's state changes.
-                //    The function indicates that when playing a video (state=1),
-                //    the player should play for six seconds and then stop.
-                var done = false;
-
-                function onPlayerStateChange(event) {
-                    if (event.data == YT.PlayerState.PLAYING && !done) {
-                        setTimeout(stopVideo, 6000);
-                        done = true;
-                    }
+                function pauseVideo() {
+                    player.pauseVideo();
                 }
 
-                function stopVideo() {
-                    player.stopVideo();
+                function playVideo() {
+                    player.playVideo();
                 }
-
-                window.addEventListener("storage", function() {
-                    if (localStorage.getItem('panggil')==1){
-                        stopVideo();
-                    }
-                }, false);
             </script>
 
 
