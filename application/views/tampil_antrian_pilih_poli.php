c24e4ae837f90f4ef5324395f918fae757fe930d
ritotombe <ritotombe@gmail.com>
2020-08-26 21:38:16 +1000

    stop video

--- application/views/tampil_antrian_pilih_poli.php
+++ application/views/tampil_antrian_pilih_poli.php
@@ -203,10 +203,20 @@ defined('BASEPATH') or exit('No direct script access allowed');
             <script src="<?php echo base_url(); ?>assets/layouts/global/scripts/quick-nav.min.js" type="text/javascript"></script>
             <!-- END THEME LAYOUT SCRIPTS -->
             <script type="text/javascript">
+                 var player;
                 $(document).ready(function() {
                     setInterval(function() {
                         $('#antrian_container').load(document.URL + ' #antrian_container');
                     }, 1000);
+
+                    function onYouTubeIframeAPIReady() {
+                    console.log("cek-1");
+                    player = new YT.Player('player', {
+                        events: {
+                            'onReady': onPlayerReady
+                        }
+                    });
+                }
 
                 });
             </script>
@@ -217,16 +227,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
                 var firstScriptTag = document.getElementsByTagName('script')[0];
                 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-                var player;
-
-                function onYouTubeIframeAPIReady() {
-                    console.log("cek-1");
-                    player = new YT.Player('player', {
-                        events: {
-                            'onReady': onPlayerReady
-                        }
-                    });
-                }
+               
 
                 function onPlayerReady(event) {
                     console.log("cek0");
